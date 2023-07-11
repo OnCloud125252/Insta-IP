50f3acdfd7adf52a68cda4e03e50a4d616f472b1
OnCloud <anonymousaaaa41414141@gmail.com>
2023-07-12 04:12:01 +0800

    Add icon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="apple-touch-icon" sizes="110x110" href="/images/favicon.png" />
+                <link rel="icon" type="image/png" sizes="any" href="/images/favicon.png" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
