5a81f4356e9faa6445d664f8716d7c3b43f77aee
OnCloud <anonymousaaaa41414141@gmail.com>
2023-07-12 04:40:37 +0800

    Add Open Graph and update icon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,30 @@ export default function Document() {
             <Head>
                 <link rel="apple-touch-icon" sizes="110x110" href="/images/favicon.png" />
                 <link rel="icon" type="image/png" sizes="any" href="/images/favicon.png" />
+
+                <meta name="author" content="OnCloud" />
+                <meta name="theme-color" content="#0a1929" />
+                <meta name="description" content="Get your IP address and browser information!" />
+                <meta name="copyright" content="Copyright (c) by OnCloud" />
+
+                <meta httpEquiv="content-Type" content="text/html; charset=utf-8" />
+                <meta httpEquiv="content-language" content="zh-TW" />
+                <meta httpEquiv="widow-target" content="_top" />
+
+                {/* <!-- Twitter --> */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="What is my IP?" />
+                <meta name="twitter:description" content="Get your IP address and browser information!" />
+                <meta name="twitter:site" content="@" />
+                <meta name="twitter:creator" content="@" />
+                <meta name="twitter:image" content="/images/logo.png" />
+
+                {/* <!-- Open Graph --> */}
+                <meta property="og:url" content="https://ip.on-cloud.tw/" />
+                <meta property="og:title" content="What is my IP?" />
+                <meta property="og:type" content="website" />
+                <meta property="og:description" content="Get your IP address and browser information!" />
+                <meta property="og:image" content="/images/logo.png" />
             </Head>
             <body>
                 <Main />
